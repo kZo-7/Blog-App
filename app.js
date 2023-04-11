7b7c8cc848cf039bfc775d43890b28b49b0b1336
kZo-7 <kgkriz.bsn@gmail.com>
2023-04-11 23:21:50 +0300

    Setting up functionality & adding  Lodash module

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
+const _ = require("lodash");
 
 const homeStartingContent = "Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.";
 const aboutContent = "Hac habitasse platea dictumst vestibulum rhoncus est pellentesque. Dictumst vestibulum rhoncus est pellentesque elit ullamcorper. Non diam phasellus vestibulum lorem sed. Platea dictumst quisque sagittis purus sit. Egestas sed sed risus pretium quam vulputate dignissim suspendisse. Mauris in aliquam sem fringilla. Semper risus in hendrerit gravida rutrum quisque non tellus orci. Amet massa vitae tortor condimentum lacinia quis vel eros. Enim ut tellus elementum sagittis vitae. Mauris ultrices eros in cursus turpis massa tincidunt dui.";
@@ -12,10 +13,12 @@ app.set('view engine', 'ejs');
 
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
+app.locals._ = _;
 
 var posts = [];
 
 app.get("/", (req, res) => {
+  // const truncatedContent = _.truncate(posts.content);
   res.render("home", {
     homeCont: homeStartingContent,
     postsComposed: posts
@@ -46,8 +49,26 @@ app.post("/compose", (req, res) => {
   res.redirect("/");
 });
 
+app.get("/posts/:postHeading", (req, res) => {
+  const requestedTitle = _.kebabCase(req.params.postHeading);
 
+  posts.forEach((post) => {
+    const kebabStoredTitle = _.kebabCase(post.title);
+    const title = post.title;
+    const content = post.content;
 
+    if (requestedTitle === kebabStoredTitle) {
+      console.log("Match found.");
+      res.render("post", {
+        postTitle: title,
+        kebabPostTitle: kebabStoredTitle,
+        postContent: content
+      });
+    } else {
+      console.log("Not found.");
+    }
+  });
+});
 
 app.listen(3000, function() {
   console.log("Server started on port 3000");
